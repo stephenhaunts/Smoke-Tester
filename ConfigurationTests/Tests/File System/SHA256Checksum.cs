8bb2c5f2ba1d05d042627a7993e60503d4301ff6
stephenhaunts_cp <SND\stephenhaunts_cp>
2014-06-09 13:28:28 +0000

    Add test to perform a SHA256 check on a file.

---
+++ ConfigurationTests/Tests/File System/SHA256Checksum.cs
@@ -0,0 +1,60 @@
+/**
+* Smoke Tester Tool : Post deployment smoke testing tool.
+* 
+* http://www.stephenhaunts.com
+* 
+* This file is part of Smoke Tester Tool.
+* 
+* Smoke Tester Tool is free software: you can redistribute it and/or modify it under the terms of the
+* GNU General Public License as published by the Free Software Foundation, either version 2 of the
+* License, or (at your option) any later version.
+* 
+* Smoke Tester Tool is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* 
+* See the GNU General Public License for more details <http://www.gnu.org/licenses/>.
+* 
+* Curator: Stephen Haunts
+*/
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using ConfigurationTests.Attributes;
+using System.ComponentModel;
+
+namespace ConfigurationTests.Tests
+{
+    public class SHA256Checksum : Test
+    {        
+        [MandatoryField]
+        [Description("Filename to check")]
+        public string Filename { get; set; }
+
+        [MandatoryField]
+        [Description("SHA256 Checksum")]
+        public string Checksum { get; set; }
+
+
+        public override void Run()
+        {
+            using (var stream = File.OpenRead(Filename))
+            {
+                var sha = new SHA256Managed();
+                var checksum = sha.ComputeHash(stream);
+                var fileChecksum = BitConverter.ToString(checksum).Replace("-", string.Empty).ToLower();
+
+                AssertState.Equal(Checksum.ToLower(), fileChecksum);
+            }
+        }
+
+        public override List<Test> CreateExamples()
+        {
+           return new List<Test>
+            {
+                new SHA256Checksum{Checksum = "4563FD", Filename = @"c:\foo.txt", TestName = "Check file checksum"}
+            };
+        }
+    }
+}
