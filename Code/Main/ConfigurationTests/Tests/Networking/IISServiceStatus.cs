fa95a937d6dfeb4bc57dd50ad40c0c3fe54b805e
stephenhaunts_cp <SND\stephenhaunts_cp>
2014-06-09 14:25:48 +0000

    Add test to check the status of IIS.

---
+++ Code/Main/ConfigurationTests/Tests/Networking/IISServiceStatus.cs
@@ -0,0 +1,51 @@
+/**
+* Smoke Tester Tool : Post deployment smoke testing tool.
+* 
+* http://www.stephenhaunts.com
+* 
+* This file is part of Smoke Tester Tool.
+* 
+* Smoke Tester Tool is free software: you can redistribute it and/or modify it under the terms of the
+* GNU General Public License as published by the Free Software Foundation, either version 2 of the
+* License, or (at your option) any later version.
+* 
+* Smoke Tester Tool is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* 
+* See the GNU General Public License for more details <http://www.gnu.org/licenses/>.
+* 
+* Curator: Stephen Haunts
+*/
+
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.ComponentModel;
+using ConfigurationTests.Attributes;
+
+namespace ConfigurationTests.Tests
+{
+    public class IISServiceStatus : Test
+    {
+        [Description("Expected Service Status")]
+        [MandatoryField]
+        public ServiceControllerStatus ServiceStatus { get; set; }
+
+        public override void Run()
+        {
+            var ctl = ServiceController.GetServices().FirstOrDefault(s => s.ServiceName == "W3SVC");
+
+            if (ctl == null) throw new AssertionException(@"IIS is not installed.");
+
+            AssertState.Equal(ServiceStatus, ctl.Status);
+        }
+
+        public override List<Test> CreateExamples()
+        {
+            return new List<Test> { new IISServiceStatus()
+            {                
+                ServiceStatus = ServiceControllerStatus.Running
+            } };
+        }
+    }
+}
