2bb8da4059e629d548bdd7a7fd14255da7bb46fa
stephenhaunts_cp <SND\stephenhaunts_cp>
2014-06-19 08:09:49 +0000

    Populate text based test execution report.

--- Code/Main/CommonCode/ReportWriter/ReportTypes/TextReport.cs
+++ Code/Main/CommonCode/ReportWriter/ReportTypes/TextReport.cs
@@ -16,14 +16,66 @@
 * 
 * Curator: Stephen Haunts
 */
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 namespace CommonCode.ReportWriter.ReportTypes
 {
     public class TextReport : IReportType
    {
         public void WriteReport(string fileName, List<ReportEntry> reportEntries)
-        {            
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentNullException("fileName");
+            }
+
+            if (reportEntries.Count == 0)
+            {
+                throw new ArgumentNullException("reportEntries");
+            }
+
+            var textReport = new StringBuilder();
+            textReport.AppendLine("Report Generated : " + DateTime.Now);
+            textReport.AppendLine("Test Execution Machine : " + System.Environment.MachineName);
+            textReport.AppendLine("");
+
+            var count = 1;
+
+            foreach (var entry in reportEntries)
+            {
+                textReport.AppendLine("Test Number : " + count);
+                textReport.AppendLine("-----------------");
+                textReport.AppendLine("");
+                textReport.AppendLine("Test Name : " + entry.TestName);
+
+                var result = "Failed";
+
+                if (entry.Result)
+                {
+                    result = "Passed";
+                }
+
+                textReport.AppendLine("Test Result : " + result);
+                textReport.AppendLine("Test Start Time : " + entry.TestStartTime);
+                textReport.AppendLine("Test Stop Time : " + entry.TestStopTime);
+
+                if (!entry.Result)
+                {
+                    textReport.AppendLine("Error Message : " + entry.ErrorMessage);
+                }
+
+                textReport.AppendLine("");
+                textReport.AppendLine("");
+
+                count++;
+            }
+
+            var file = new StreamWriter(fileName);
+            file.WriteLine(textReport.ToString());
+            file.Close();         
         }
     }
 }
